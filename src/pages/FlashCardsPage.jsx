680b8645e2c49c3441fd65c5d1f52dd30f3dc0c2
filipef9 <filipef9@hotmail.com>
2021-07-31 04:21:14 +0000

    Implmentação da feature de embaralhamento dos cards.

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -3,14 +3,26 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import FlashCards from '../components/FlashCards';
 import { allFlashCards } from '../data/allFlashCards';
+import Button from '../components/Button';
+import { useState } from 'react';
+import { helperShuffleArray } from '../helpers/arrayHelpers';
 
 const FlashCardsPage = () => {
+  const [allCards, setAllCards] = useState(allFlashCards);
+
+  const handleButtonClick = () => {
+    setAllCards(helperShuffleArray(allCards));
+  };
+
   return (
     <>
       <Header>react-flash-cards-v1</Header>
       <Main>
+        <div className="text-center mb-4">
+          <Button onButtonClick={handleButtonClick}>Embaralhar cards</Button>
+        </div>
         <FlashCards>
-          {allFlashCards.map(({ id, title, description }) => (
+          {allCards.map(({ id, title, description }) => (
             <FlashCard key={id} title={title} description={description} />
           ))}
         </FlashCards>
