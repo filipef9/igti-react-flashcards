926ccca9d4a7fd17f1790ea1be8d7b2534067769
filipef9 <filipef9@hotmail.com>
2021-07-31 03:08:31 +0000

    Refactoring da página FlashCardsPage.

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -1,20 +1,12 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+
 const FlashCardsPage = () => {
   return (
-    <div>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Projeto base para o Módulo React I
-          </h1>
-        </div>
-      </header>
-
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
-    </div>
+    <>
+      <Header>react-flash-cards-v1</Header>
+      <Main>Conteúdo</Main>
+    </>
   );
 };
 
