3f2f9353f56ebca49e2f4e4d2a551424977e8296
filipef9 <filipef9@hotmail.com>
2021-07-31 03:47:41 +0000

    Implementação da renderização da lista de flashcards.

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,10 +1,30 @@
-const FlashCard = () => {
+import { useState } from 'react';
+
+const FlashCard = ({
+  title = 'Título do card',
+  description = 'Descrição do card',
+}) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  const fontSizeClassName = showTitle ? 'text-xl' : 'text-md';
+
+  const handleCardClick = () => {
+    setShowTitle(currentShowTitle => !currentShowTitle);
+  };
+
   return (
     <div
-      className="border p-4 w-64 h-32 flex items-center justify-center font-semibold text-xl"
+      className={`
+        shadow-lg
+        cursor-pointer
+        p-4 w-64 h-32
+        flex items-center justify-center
+        font-semibold ${fontSizeClassName}
+      `}
       style={{ fontFamily: "'JetBrains Mono', monospace" }}
+      onClick={handleCardClick}
     >
-      FlashCard
+      {showTitle ? title : description}
     </div>
   );
 };
--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -1,13 +1,16 @@
 import FlashCard from '../components/FlashCard';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import { allFlashCards } from '../data/allFlashCards';
 
 const FlashCardsPage = () => {
   return (
     <>
       <Header>react-flash-cards-v1</Header>
       <Main>
-        <FlashCard />
+        {allFlashCards.map(({ id, title, description }) => (
+          <FlashCard key={id} title={title} description={description} />
+        ))}
       </Main>
     </>
   );
